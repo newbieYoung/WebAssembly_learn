536755a5ef4cb4c11f73b9800b9ee7d9476608c5
NewYoung <657851574@qq.com>
2019-10-23 20:43:37 +0800

    assemblyscript 获取以及传递 字符串 例子

--- assembly/index.ts
+++ assembly/index.ts
@@ -7,3 +7,11 @@ export function add(a: i32, b: i32): i32 {
 export function minus(a: i32, b: i32): i32 {
   return a - b
 }
+
+export function hello(): string {
+  return 'hello webassembly'
+}
+
+export function hi(name: string): string {
+  return 'hi ' + name
+}
