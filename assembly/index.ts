c72b62c3275a955ba47f6101b5bdfa19cc550573
NewYoung <657851574@qq.com>
2019-10-28 16:17:12 +0800

    Mah 对象使用

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,5 +1,6 @@
 // The entry file of your WebAssembly module.
 // javascript 并不支持 64位 数据类型，因此在 assemblyscript 中使用 64 位数据类型并没有任何问题，但是在 javascript 中调用时则会报错。
+NativeMath.seedRandom(Date.now())
 
 export function add(a: i32, b: i32): i32 {
   return a + b
@@ -15,6 +16,18 @@ export function hello(): string {
 
 export function hi(name: string): string {
   return 'hi ' + name
+}
+
+/**
+ * 要想在 assemblyscript 中使用 javascript 的 Math 需要在编译时指定 --use Math=JSMath；
+ * 否则默认是 NativeMath 而 NativeMath 的方法 和 Math 有一些小的差别，比如：NativeMath.random 获取随机数之前需要先执行 NativeMath.seedRandom。
+ */
+export function random(): f32 {
+  return <f32>NativeMath.random()
+}
+
+export function dateNow(): i32 {
+  return <i32>Date.now()
 }
 
 //计算斐波那契数列
