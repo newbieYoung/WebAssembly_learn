99b2ac6e005392c69a635113b9b8bbc82486dce1
NewYoung <657851574@qq.com>
2019-10-31 21:22:08 +0800

    小修改

--- browser/index.js
+++ browser/index.js
@@ -16,6 +16,13 @@ let myImport = {
 }
 loader.preInstantiate(myImport);
 
+//计算斐波那契数列
+function fib(n) {
+  if (n < 2) {
+    return 1
+  }
+  return fib(n - 2) + fib(n - 1)
+}
 
 fetch("../build/optimized.wasm")
   .then(response => response.arrayBuffer())
